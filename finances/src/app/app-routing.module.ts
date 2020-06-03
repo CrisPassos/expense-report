e88e71157d2a53be0aad4069039b7a36c9ef053c
Cristiana Passos <cristiana.passos@bee-eng.pt>
2020-06-03 11:09:49 +0100

    include lazy loading

--- finances/src/app/app-routing.module.ts
+++ finances/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+// lazyloading
+const routes: Routes = [
+  { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
