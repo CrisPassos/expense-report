0a1a91fa99b27eb2b764a73be04cf4e70593bf35
Cristiana Passos <cristiana.passos@bee-eng.pt>
2020-06-04 21:11:58 +0100

    add catery in memory database

---
+++ finances/src/in-memoy-db.ts
@@ -0,0 +1,18 @@
+import { InMemoryDbService, httpClientInMemBackendServiceFactory } from 'angular-in-memory-web-api';
+
+export class InMemoryDatabase implements InMemoryDbService {
+
+  createDb() {
+    const categories = [
+      { id: 1, name: 'Moradia', description: 'Pagamentos de Contas de Casa' },
+      { id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios' },
+      { id: 3, name: 'Lazer', description: 'Cinemas, parques, praias, etc' },
+      { id: 4, name: 'Salário', description: 'Recebimento de Salário' },
+      { id: 5, name: 'Freelas', description: 'Trabalhos com freelancher' },
+    ];
+
+    return categories;
+  }
+
+}
+
