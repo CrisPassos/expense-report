a68b9b45c63536c2504fe4483f2565b86cae7e93
Cristiana Passos <cristiana.passos@bee-eng.pt>
2020-06-12 22:36:02 +0100

    associate category with entry

--- finances/src/app/pages/entries/shared/entry.service.ts
+++ finances/src/app/pages/entries/shared/entry.service.ts
@@ -1,3 +1,4 @@
+import { CategoryService } from './../../categories/shared/category.service';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
@@ -10,7 +11,7 @@ import { Entry } from './entry.model';
 export class EntryService {
   private apiPath = 'api/entries';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private categoryService: CategoryService) { }
 
   getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe(
@@ -29,18 +30,27 @@ export class EntryService {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handlerError),
-      map(this.jsonDataToEntry),
-    );
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handlerError),
+          map(this.jsonDataToEntry),
+        );
+      }));
   }
 
   update(entry: Entry): Observable<Entry> {
     const url = `${this.apiPath}/${entry.id}`;
-    return this.http.put(url, entry).pipe(
-      catchError(this.handlerError),
-      map(() => entry),
-    );
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+        return this.http.put(url, entry).pipe(
+          catchError(this.handlerError),
+          map(() => entry),
+        );
+      }));
   }
 
   delete(id: number): Observable<any> {
