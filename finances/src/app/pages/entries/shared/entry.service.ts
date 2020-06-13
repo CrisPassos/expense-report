faad254de7df5573282b141658fcfebfc32f7c01
Cristiana Passos <cristiana.passos@bee-eng.pt>
2020-06-13 15:00:19 +0100

    improving entry service

--- finances/src/app/pages/entries/shared/entry.service.ts
+++ finances/src/app/pages/entries/shared/entry.service.ts
@@ -3,7 +3,7 @@ import { Injectable, Injector } from '@angular/core';
 import { Entry } from './entry.model';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
 import { Observable } from 'rxjs';
-import { flatMap } from 'rxjs/operators';
+import { flatMap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,20 +15,22 @@ export class EntryService extends BaseResourceService<Entry>{
   }
 
   create(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this));
+  }
+
+  update(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+  }
+
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap(category => {
         entry.category = category;
         // só fizemos isso pq havia um relacionamento entre entry e category
-        return super.create(entry);
-      }));
-  }
-
-  update(entry: Entry): Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-        return super.update(entry);
-      }));
+        return sendFn(entry);
+      }),
+      catchError(this.handlerError)
+    );
   }
 
 }
