3524277eefdb4d26813b792aca67b2863be157da
Cristiana Passos <cristiana.passos@bee-eng.pt>
2020-06-18 16:05:50 +0100

    add default routers

--- finances/src/app/app-routing.module.ts
+++ finances/src/app/app-routing.module.ts
@@ -5,7 +5,9 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' },
   { path: 'entries', loadChildren: './pages/entries/entries.module#EntriesModule' },
-  { path: 'reports', loadChildren: './pages/reports/reports.module#ReportsModule' }
+  { path: 'reports', loadChildren: './pages/reports/reports.module#ReportsModule' },
+
+  { path: '', redirectTo: '/reports', pathMatch: 'full' }
 ];
 
 @NgModule({
