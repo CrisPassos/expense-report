85bb3c3123244eae1cd010bd88d66f1709aa5eb2
Cristiana Passos <cristiana.passos@bee-eng.pt>
2020-06-18 11:19:19 +0100

    insert navbar

--- finances/src/app/core/core.module.ts
+++ finances/src/app/core/core.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
@@ -7,24 +8,29 @@ import { HttpClientModule } from '@angular/common/http';
 // memory-api: todas as requisicoes serao interceptadas pelo HttpClientInMemory
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from '../in-memoy-db';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 // serviços e modulos obrigatório para o sistema funcionar
 // exemplo header, footer
 // singleton, auth guards routes, services (token service)
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent
+  ],
   imports: [
     CommonModule,
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+    RouterModule
   ],
   exports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    NavbarComponent,
   ],
 })
 export class CoreModule { }
